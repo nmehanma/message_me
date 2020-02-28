03554901de02a6bcc19eae253ae43ad0e077afd7
nmehanma <nmehanma@gmail.com>
2020-02-27 22:43:10 -0500

    update bugs in code

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,11 @@ Rails.application.routes.draw do
 
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
-<<<<<<< HEAD
-<<<<<<< HEAD
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
 
   mount ActionCable.server, at: '/cable'
-
-<<<<<<< HEAD
-=======
->>>>>>> parent of f789e26... adding create and destroy sessions for authentication systems
-=======
->>>>>>> 8e5644dd17c48d86b6bf7133cf280e65ed9def66
-=======
->>>>>>> parent of f789e26... adding create and destroy sessions for authentication systems
 
   
 end 
